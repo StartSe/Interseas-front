2407d75920a173cb5c0f2999698efe0a48eeba2d
VitorNakagawaL <vitor.nakagawa@startse.com>
2024-08-30 15:53:08 -0300

    refactor: project structure

---
+++ flowise-embed/dist/utils/fileClassificationUtils.d.ts
@@ -0,0 +1,66 @@
+export declare const checklistCeMercante = "\n\u2022 NCM\n\u2022 Importador\n\u2022 Adquirente\n\u2022 Peso Bruto\n\u2022 Data de embarque\n\u2022 Local de Embarque\n\u2022 Valores Lan\u00E7ados (Prepaid/Collect; moeda; valor)\n\u2022 Consulta benef\u00EDcios isen\u00E7\u00E3o/suspens\u00E3o do AFRMM - AI\n\u2022 Solicitar inclus\u00E3o dos valores de Capatazia/THC no BL";
+export declare const checklistCertificadoOrigem = "\n\u2022 Dados do Exportador\n\u2022 Dados do Importador\n\u2022 Dados do Consignat\u00E1rio\n\u2022 Acordo\n\u2022 Fatura Comercial\n\u2022 Valor da mercadoria\n\u2022 Quantidade\n\u2022 NCM\nSe opera\u00E7\u00E3o por Conta e Ordem:\n\u2022 Dados do Adquirente (no campo Observa\u00E7\u00F5es)";
+export declare const checklistCommercialInvoice = "\n\u2022 N\u00FAmero do documento\n\u2022 Nome do documento\n\u2022 Data\n\u2022 Assinatura \u00E0 m\u00E3o\n\u2022 Dados do Importador - tamb\u00E9m chamado de Consignee (Raz\u00E3o social, endere\u00E7o e CNPJ)\n\u2022 Dados do Adquirente - tamb\u00E9m chamado de Buyer (se importa\u00E7\u00E3o por Conta e Ordem ou por Encomenda)\n\u2022 Dados do Exportador (nome, endere\u00E7o, NIF)\n\u2022 Dados do Fabricante (nome, endere\u00E7o, NIF)\n\u2022 Marca\n\u2022 Numera\u00E7\u00E3o\n\u2022 N\u00FAmero de refer\u00EAncia dos volumes\n\u2022 Descri\u00E7\u00E3o\n\u2022 Refer\u00EAncia\n\u2022 Quantidade\n\u2022 Unidade Comercializada\n\u2022 Valor Unit\u00E1rio (n\u00E3o zerado)\n\u2022 Valor Total (n\u00E3o zerado)\n\u2022 Moeda\n\u2022 Forma/Condi\u00E7\u00F5es de Pagamento\n\u2022 Dados Banc\u00E1rios Exportador\n\u2022 Porto de Embarque\n\u2022 Porto de Desembarque\n\u2022 Pa\u00EDs de Origem\n\u2022 Pa\u00EDs Proced\u00EAncia\n\u2022 Pa\u00EDs de Aquisi\u00E7\u00E3o\n\u2022 INCOTERM\n\u2022 Local do INCOTERM\n\u2022 Valor do Frete (se prepaid)\n\u2022 Valor do Seguro (se prepaid)\n\u2022 Peso L\u00EDquido Total\n\u2022 Peso Bruto Total\n\u2022 Quantidade de Volumes\n\u2022 Tipo de Volumes\n\u2022 Descri\u00E7\u00E3o EX-tarif\u00E1rio (no formato \"EX-[n\u00FAmero]\")\nSe mercadoria \u00E9 m\u00E1quina ou equipamento\n\u2022 N\u00BA de S\u00E9rie\n\u2022 Marca\n\u2022 Modelo\nConfer\u00EAncias:\n\u2022 Multiplica\u00E7\u00E3o de valor unit\u00E1rio = quantidade comercializada de cada item\n\u2022 Somat\u00F3rio dos itens = valor total informado";
+export declare const checklistConhecimentoBL = "\n\u2022 Shipper (nome, endere\u00E7o)\n\u2022 Dados do Importador - tamb\u00E9m chamado de Consignee (raz\u00E3o social, endere\u00E7o e CNPJ)\n\u2022 Notify (raz\u00E3o social, endere\u00E7o e CNPJ)\n\u2022 Valor do Frete\n\u2022 Moeda do Frete\n\u2022 Tipo de Frete\n\u2022 N\u00FAmero dos containers (no formato <3 letras>U<7 n\u00FAmeros>)\n\u2022 N\u00FAmero dos lacres dos containers (seal)\n\u2022 Local de Recebimento\n\u2022 Porto de Embarque\n\u2022 Porto de Desembarque\n\u2022 Local de Destino\n\u2022 Peso Bruto\n\u2022 Cubagem\n\u2022 Quantidade de Volumes\n\u2022 Tipo de Volumes\n\u2022 Informa\u00E7\u00E3o \"Wooden Packing\"\n\u2022 Descri\u00E7\u00E3o resumida das mercadorias\n\u2022 NCM (primeiros 4 d\u00EDgitos)\n\u2022 Valor da Capatazia (THC)\n\u2022 Descri\u00E7\u00E3o EX-tarif\u00E1rio (no formato \"EX-[n\u00FAmero]\")\nSe mercadoria \u00E9 m\u00E1quina ou equipamento\n\u2022 N\u00BA de S\u00E9rie\nConfer\u00EAncias:\n\u2022 Se importa\u00E7\u00E3o direta:\nNotify = Importador\n\u2022 Se importa\u00E7\u00E3o por Conta e Ordem:\nNotify = Adquirente ou Importador\n\u2022 Se INCOTERM de responsabilidade do exportador:\nTipo de frete = \"Prepaid\"\n\u2022 Se INCOTERM de responsabilidade do importador:\nTipo de frete = \"Collect\"";
+export declare const checklistConhecimentoHawb = "\n\u2022 Shipper (nome, endere\u00E7o)\n\u2022 Dados do Importador - tamb\u00E9m chamado de Consignee (raz\u00E3o social, endere\u00E7o e CNPJ)\n\u2022 Notify (raz\u00E3o social, endere\u00E7o e CNPJ)\n\u2022 Valor do Frete\n\u2022 Moeda do Frete\n\u2022 Tipo de Frete\n\u2022 Aeroporto de Partida\n\u2022 Aeroporto de Destino\n\u2022 Peso Bruto\n\u2022 Peso Taxado\n\u2022 Quantidade de Volumes\n\u2022 Informa\u00E7\u00E3o \"Wooden Packing\"\n\u2022 Descri\u00E7\u00E3o resumida das mercadorias\nSe mercadoria \u00E9 m\u00E1quina ou equipamento\n\u2022 N\u00BA de S\u00E9rie\n\u2022 Se INCOTERM de responsabilidade do exportador:\nTipo de frete = \"Prepaid\"\n\u2022 Se INCOTERM de responsabilidade do importador:\nTipo de frete = \"Collect\"";
+export declare const checklistCRT = "\n\u2022 Shipper\n\u2022 Consignee\n\u2022 Notify\n\u2022 Frete\n\u2022 Tipo de Frete\n\u2022 N\u00BA Cont\u00EAineres/Lacres\n\u2022 Local de Recebimento\n\u2022 Porto | Embarque\n\u2022 Porto | Desembarque\n\u2022 Local de Destino\n\u2022 Peso Bruto\n\u2022 Cubagem\n\u2022 Quantidade de Volumes\n\u2022 Tipo de Volumes\n\u2022 Detalhamento volumes (Processed Wood/ Treated and\n\u2022 NCM (4 d\u00EDgitos)\n\u2022 N\u00BA de S\u00E9rie (obrigat\u00F3rio para m\u00E1quinas e equipamentos)\n\u2022 Processo com EX|Descri\u00E7\u00E3o Conforme o Ex-tarif\u00E1rio";
+export declare const checklistPackingList = "\n\u2022 Refer\u00EAncia \u00E0 Ordem de Compra (OC) ou Fatura Comercial\n\u2022 Dados do Importador\n\u2022 Dados do Adquirente ou Encomendante\n\u2022 Dados do Exportador\n\u2022 Descri\u00E7\u00E3o ou refer\u00EAncia\n\u2022 Esp\u00E9cie dos volumes\n\u2022 Quantidade total de Volumes\n\u2022 Peso L\u00EDquido por volume\n\u2022 Peso L\u00EDquido total\n\u2022 Peso Bruto total\n\u2022 Cubagem total";
+export declare const ChecklistProformaInvoice = "\n\u2022 N\u00FAmero do documento\n\u2022 Nome do documento\n\u2022 Data do documento\n\u2022 Assinatura\n\u2022 Dados do Importador - tamb\u00E9m chamado de Consignee (Raz\u00E3o social, endere\u00E7o e CNPJ)\n\u2022 Dados do Adquirente - tamb\u00E9m chamado de Buyer (se importa\u00E7\u00E3o por Conta e Ordem ou por Encomenda)\n\u2022 Dados do Exportador (nome, endere\u00E7o)\n\u2022 Pa\u00EDs de origem\n\u2022 Descri\u00E7\u00E3o da mercadoria\n\u2022 Quantidade\n\u2022 Unidade comercializada\n\u2022 Valor unit\u00E1rio de cada esp\u00E9cie de mercadoria\n\u2022 Valor total de cada esp\u00E9cie de mercadoria\n\u2022 Moeda de pagamento\n\u2022 Condi\u00E7\u00F5es de pagamento\n\u2022 Dados banc\u00E1rios do exportador\n\u2022 INCOTERM\n\u2022 Local do INCOTERM\n\u2022 Peso estimado\n\u2022 Quntidade de volumes estimada\n\u2022 Dimens\u00E3o estimada dos volumes\nConfer\u00EAncias:\n\u2022 Multiplica\u00E7\u00E3o do valor unit\u00E1rio = quantidade comercializada de cada item\n\u2022 Somat\u00F3rio dos itens = valor total informado";
+export declare enum DocumentTypes {
+    PROFORMA_INVOICE = "PROFORMA INVOICE",
+    COMMERCIAL_INVOICE = "COMMERCIAL INVOICE",
+    PACKING_LIST = "PACKING LIST",
+    CONHECIMENTO_BL = "CONHECIMENTO - B/L",
+    CONHECIMENTO_HAWB = "HAWB",
+    CONHECIMENTO_MAWB = "CONHECIMENTO - MAWB",
+    CONHECIMENTO_CRT = "CONHECIMENTO - CRT",
+    CONHECIMENTO_MIC_DTA = "CONHECIMENTO - MIC/DTA",
+    CE_MERCANTE = "CE MERCANTE",
+    CCT = "CCT",
+    INSTRUCAO_DE_EMBARQUE = "INSTRU\u00C7\u00C3O DE EMBARQUE",
+    DUIMP = "DUIMP",
+    DUE = "DU-E",
+    DECLARACAO_DE_IMPORTACAO = "DECLARA\u00C7\u00C3O DE IMPORTA\u00C7\u00C3O",
+    RESUMO_DA_DECLARACAO_DE_IMPORTACAO = "RESUMO DA DECLARA\u00C7\u00C3O DE IMPORTA\u00C7\u00C3O",
+    LICENCA_DE_IMPORTACAO = "LICEN\u00C7A DE IMPORTA\u00C7\u00C3O",
+    LPCO = "LPCO (LICEN\u00C7AS, PERMISS\u00D5ES, CERTIFICADOS E OUTROS)",
+    DOWNPAYMENT_INVOICE = "DOWNPAYMENT INVOICE",
+    PROPOSTA = "PROPOSTA",
+    ORDEM_DE_COMPRA_DO_IMPORTADOR = "ORDEM DE COMPRA DO IMPORTADOR",
+    SALES_ORDER_DOCUMENT = "SALES ORDER DOCUMENT",
+    CONFIRMATION_OF_ORDER = "CONFIRMATION OF ORDER",
+    CERTIFICADO_DE_ORIGEM_DIGITAL = "CERTIFICADO DE ORIGEM DIGITAL",
+    CERTIFICADO_DE_ORIGEM = "CERTIFICADO DE ORIGEM",
+    TEST_REPORT = "TEST REPORT",
+    LABELS = "LABEL",
+    ANEXO_IX = "ANEXO IX - CERTIFICADO DE ORIGEM DE BEBIDAS, FERMENTADOS AC\u00C9TICOS, VINHOS E DERIVADOS DA UVA E DO VINHO PARA O BRASIL",
+    ANEXO_XI = "ANEXO XI - COMPROVA\u00C7\u00C3O OFICIAL DE TIPICIDADE E REGIONALIDADE DE BEBIDAS ALCO\u00D3LICAS, VINHOS E DERIVADOS DA UVA E DO VINHO PARA IMPORTA\u00C7\u00C3O PELO BRASIL",
+    CERTIFICADO_DE_INSPECAO = "CERTIFICADO DE INSPE\u00C7\u00C3O DE IMPORTA\u00C7\u00C3O DE BEBIDAS, FERMENTADOS AC\u00C9TICOS, VINHOS E DERIVADOS DA UVA E DO VINHO",
+    CERTIFICADO_DE_CONFORMIDADE_ORGANICA = "CERTIFICADO DE CONFORMIDADE ORG\u00C2NICA",
+    DECLARACAO_DE_TRANSACAO_COMERCIAL = "DECLARA\u00C7\u00C3O DE TRANSA\u00C7\u00C3O COMERCIAL",
+    ATESTADO_DE_INEXISTENCIA_DE_PRODUCAO_ESTADUAL = "ATESTADO DE INEXIST\u00CANCIA DE PRODU\u00C7\u00C3O ESTADUAL",
+    CATALOGO_DE_EQUIPAMENTO = "CATALOGO DE EQUIPAMENTO",
+    CERTIFICADO_DE_COMPLIANCE = "CERTIFICADO DE COMPLIANCE",
+    CERTIFICADO_DE_ESTERILIZACAO = "CERTIFICADO DE ESTERILIZA\u00C7\u00C3O",
+    DECLARACAO_DO_DETENTOR_DA_REGULARIZACAO = "DECLARA\u00C7\u00C3O DO DETENTOR DA REGULARIZA\u00C7\u00C3O",
+    MSDS = "MSDS",
+    FICHA_DE_EMERGENCIA = "FICHA DE EMERG\u00CANCIA",
+    FISPQ = "FISPQ",
+    SHIPPERS_DECLARATION = "SHIPPER'S DECLARATION FOR DANGEROUS GOODS",
+    ANEXO_VII = "DECLARA\u00C7\u00C3O DE CARGA PERIGOSA",
+    FICHA_DE_LOTE = "FICHA DE LOTE",
+    CERTIFICADO_FITOSSANITARIO = "CERTIFICADO FITOSSANIT\u00C1RIO"
+}
+export declare const checklistTypeMapping: {
+    "PROFORMA INVOICE": string;
+    "COMMERCIAL INVOICE": string;
+    "PACKING LIST": string;
+    "CONHECIMENTO - B/L": string;
+    HAWB: string;
+    "CONHECIMENTO - CRT": string;
+    "CE MERCANTE": string;
+    "CERTIFICADO DE ORIGEM": string;
+};
+export declare const identifyDocumentChecklist: (documentType: keyof typeof DocumentTypes) => string | null;
+export declare const identifyDocumentType: (fileName: string) => "CCT" | "DUIMP" | "PROPOSTA" | "MSDS" | "FISPQ" | "PROFORMA_INVOICE" | "COMMERCIAL_INVOICE" | "PACKING_LIST" | "CONHECIMENTO_BL" | "CONHECIMENTO_HAWB" | "CONHECIMENTO_MAWB" | "CONHECIMENTO_CRT" | "CONHECIMENTO_MIC_DTA" | "CE_MERCANTE" | "INSTRUCAO_DE_EMBARQUE" | "DUE" | "DECLARACAO_DE_IMPORTACAO" | "RESUMO_DA_DECLARACAO_DE_IMPORTACAO" | "LICENCA_DE_IMPORTACAO" | "LPCO" | "DOWNPAYMENT_INVOICE" | "ORDEM_DE_COMPRA_DO_IMPORTADOR" | "SALES_ORDER_DOCUMENT" | "CONFIRMATION_OF_ORDER" | "CERTIFICADO_DE_ORIGEM_DIGITAL" | "CERTIFICADO_DE_ORIGEM" | "TEST_REPORT" | "LABELS" | "ANEXO_IX" | "ANEXO_XI" | "CERTIFICADO_DE_INSPECAO" | "CERTIFICADO_DE_CONFORMIDADE_ORGANICA" | "DECLARACAO_DE_TRANSACAO_COMERCIAL" | "ATESTADO_DE_INEXISTENCIA_DE_PRODUCAO_ESTADUAL" | "CATALOGO_DE_EQUIPAMENTO" | "CERTIFICADO_DE_COMPLIANCE" | "CERTIFICADO_DE_ESTERILIZACAO" | "DECLARACAO_DO_DETENTOR_DA_REGULARIZACAO" | "FICHA_DE_EMERGENCIA" | "SHIPPERS_DECLARATION" | "ANEXO_VII" | "FICHA_DE_LOTE" | "CERTIFICADO_FITOSSANITARIO" | null;
+//# sourceMappingURL=fileClassificationUtils.d.ts.map

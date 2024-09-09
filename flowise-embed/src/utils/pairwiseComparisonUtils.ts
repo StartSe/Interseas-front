33e12859544f1623a1972049df869973f2cdaba0
VitorNakagawaL <vitor.nakagawa@startse.com>
2024-09-09 16:40:37 -0300

    feat: filter specific compliance methods using document type

--- flowise-embed/src/utils/pairwiseComparisonUtils.ts
+++ flowise-embed/src/utils/pairwiseComparisonUtils.ts
@@ -1,7 +1,24 @@
 import { FileMapping } from '@/utils/fileUtils';
+import { DocumentTypes } from './fileClassificationUtils';
 
 export function pairwiseCompareDocuments(fileMappings: FileMapping[], comparePair: (firstFile: FileMapping, secondFile: FileMapping) => void): void {
   const processedPairs: Set<string> = new Set();
+
+  comparePair = (firstFile, secondFile) => {
+    if (
+      (firstFile.type === DocumentTypes.CCT && secondFile.type === DocumentTypes.CONHECIMENTO_HAWB) ||
+      (firstFile.type === DocumentTypes.CONHECIMENTO_HAWB && secondFile.type === DocumentTypes.CCT)
+    ) {
+      console.log('Specific compliance');
+    } else if (
+      (firstFile.type === DocumentTypes.CE_MERCANTE && secondFile.type === DocumentTypes.CONHECIMENTO_BL) ||
+      (firstFile.type === DocumentTypes.CONHECIMENTO_BL && secondFile.type === DocumentTypes.CE_MERCANTE)
+    ) {
+      console.log('Second compliance');
+    } else {
+      console.log('Files passed the validation');
+    }
+  };
 
   fileMappings.forEach((firstFileMappingToCompare, index) => {
     for (let nextIndex = index + 1; nextIndex < fileMappings.length; nextIndex++) {

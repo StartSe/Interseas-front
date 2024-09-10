0252f42cc697e6af986f5e14d5c1da58d8b156e6
VitorNakagawaL <vitor.nakagawa@startse.com>
2024-09-10 16:48:24 -0300

    feat: call loading bubble and add commercial invoice compliance

--- flowise-embed/dist/utils/pairwiseComparisonUtils.d.ts
+++ flowise-embed/dist/utils/pairwiseComparisonUtils.d.ts
@@ -1,3 +1,7 @@
 import { FileMapping } from '@/utils/fileUtils';
+<<<<<<< HEAD
 export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], sendBackgroundMessage: (value: string, url: any) => Promise<any>, setMessages: (value: any) => void, crossValidation: (firstFile: FileMapping, secondFile: FileMapping) => Promise<void>): Promise<void>;
+=======
+export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], comparePair: (firstFile: FileMapping, secondFile: FileMapping) => void, sendMessage: (value: string, url: any) => Promise<any>, setMessages: (value: any) => void, setLoading: (value: boolean) => void): void;
+>>>>>>> 4b9e5b7 (feat: call loading bubble and add commercial invoice compliance)
 //# sourceMappingURL=pairwiseComparisonUtils.d.ts.map

6f71fe1dbdb28f8defd0155db4c493939babc245
Luís Lima <luis.lima@startse.com>
2024-09-12 17:22:38 -0300

    cleanup: remove console log

--- flowise-embed/src/components/Bot.tsx
+++ flowise-embed/src/components/Bot.tsx
@@ -1170,8 +1170,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
       const validationResponse = await sendBackgroundMessage(crossValidationPrompt, []);
 
       const extractedJson = validationResponse.text.replace(/```json|```/g, '');
-
-      console.log(`JSON Extraído:\n${extractedJson}`);
     });
   };
 

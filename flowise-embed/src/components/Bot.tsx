4df0528b43506ae54401d445eedd4198f78875d6
VitorNakagawaL <vitor.nakagawa@startse.com>
2024-09-13 16:23:33 -0300

    refactor: remove logs

--- flowise-embed/src/components/Bot.tsx
+++ flowise-embed/src/components/Bot.tsx
@@ -1086,7 +1086,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
     const result = await sendBackgroundMessage(checklistPrompt, urls);
 
     try {
-      console.log(result);
       let jsonData = JSON.parse(result.text);
       jsonData = sanitizeJson(jsonData);
 

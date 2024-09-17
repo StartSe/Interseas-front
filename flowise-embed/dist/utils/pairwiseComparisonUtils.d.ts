edc9c363c4efafc0e7ae2d4882a1285ec8bf30ac
VitorNakagawaL <vitor.nakagawa@startse.com>
2024-09-17 16:48:30 -0300

    refactor: loading compliance

--- flowise-embed/dist/utils/pairwiseComparisonUtils.d.ts
+++ flowise-embed/dist/utils/pairwiseComparisonUtils.d.ts
@@ -1,6 +1,7 @@
 import { FileMapping } from '@/utils/fileUtils';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], sendBackgroundMessage: (value: string, url: any) => Promise<any>, setMessages: (value: any) => void, crossValidation: (firstFile: FileMapping, secondFile: FileMapping) => Promise<void>): Promise<void>;
 =======
 export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], comparePair: (firstFile: FileMapping, secondFile: FileMapping) => void, sendMessage: (value: string, url: any) => Promise<any>, setMessages: (value: any) => void, setLoading: (value: boolean) => void): void;
@@ -8,4 +9,7 @@ export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], co
 =======
 export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], sendBackgroundMessage: (value: string, url: any) => Promise<any>, setMessages: (value: any) => void): Promise<void>;
 >>>>>>> af64801 (refactor: specific compliance check)
+=======
+export declare function pairwiseCompareDocuments(fileMappings: FileMapping[], sendBackgroundMessage: (value: string, url: any) => Promise<any>, setMessages: (value: any) => void, crossValidationMethod: (firstFileMapping: FileMapping, secondFileMapping: FileMapping) => Promise<void>): Promise<void>;
+>>>>>>> 349d60a (refactor: loading compliance)
 //# sourceMappingURL=pairwiseComparisonUtils.d.ts.map

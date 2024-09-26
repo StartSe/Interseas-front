93fa405ad950ba8ff7930f5839b1cc922ce28f70
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-09-26 15:12:47 -0300

    refactor: extract logic to a separate file and implement as a class

--- flowise-embed/src/utils/parallelApiExecutor.ts
+++ flowise-embed/src/utils/parallelApiExecutor.ts
@@ -1,20 +1,26 @@
 import { MessageType } from '@/components/Bot';
-class ApiRequester {
-  urls = [
+export default class ParallelApiExecutor {
+  n8nUrls = [
     'https://n8n.startse.com/webhook/a96d9f14-3835-4e08-b8a7-a77f3b754886',
     'https://n8n.startse.com/webhook/0164614d-e044-4c3f-9ef9-9212407bf402',
-    // Adicione suas outras URLs aqui
   ];
 
-  async makeRequests(jsonCriticalAnalysisUpdate: any, setMessages: (message: MessageType) => void) {
-    const requests = this.urls.map(async (url) => {
+  constructor(
+    private dependencies: {
+      jsonCriticalAnalysisUpdate: { text: string };
+      setMessages: (value: any) => void;
+    },
+  ) {}
+
+  public async execute(): Promise<void> {
+    const requests = this.n8nUrls.map(async (url) => {
       try {
         const response = await fetch(url, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(jsonCriticalAnalysisUpdate.text),
+          body: JSON.stringify(this.dependencies.jsonCriticalAnalysisUpdate.text),
         });
 
         if (!response.ok) {
@@ -24,7 +30,7 @@ class ApiRequester {
         const data = await response.json();
         const message = { message: data.output, type: 'apiMessage' } as MessageType;
 
-        setMessages(message);
+        this.sendMessageToChat(message);
       } catch (error) {
         console.error('Error:', error);
       }
@@ -32,6 +38,8 @@ class ApiRequester {
 
     await Promise.allSettled(requests);
   }
-}
 
-export default ApiRequester;
+  private sendMessageToChat(message: any): void {
+    this.dependencies.setMessages((prevMessages: any) => [...prevMessages, message]);
+  }
+}

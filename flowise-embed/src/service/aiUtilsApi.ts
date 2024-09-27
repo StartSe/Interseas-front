98f4ac7e92b5aa205350a2a8b77c4b35f8a8886a
Renato Barbosa Candido <renato.barbosa@startse.com>
2024-09-27 14:17:07 -0300

    fix: endpoint to plain-text

--- flowise-embed/src/service/aiUtilsApi.ts
+++ flowise-embed/src/service/aiUtilsApi.ts
@@ -4,7 +4,7 @@ const apiHost = constants.apiUtilsUrl;
 export const pdfToText = async (file: Blob): Promise<Response> => {
   const form = new FormData();
   form.append('file', file);
-  return fetch(`${apiHost}/text-extraction/pdf-to-text`, {
+  return fetch(`${apiHost}/text-extraction/plain-text`, {
     method: 'POST',
     body: form,
   });

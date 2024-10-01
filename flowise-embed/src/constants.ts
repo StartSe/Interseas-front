d648519e7ebbdf2643064ba911eb6bf108901c22
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-01 16:41:16 -0300

    feat: add url critical analyse step 2

--- flowise-embed/src/constants.ts
+++ flowise-embed/src/constants.ts
@@ -21,7 +21,8 @@ export const constants = {
   apiUtilsUrl: 'https://ca-ai-utils-api-prod-eastus2-001.bravepond-9830b784.eastus2.azurecontainerapps.io',
   n8nDomain: 'https://n8n.startse.com',
   n8nFirstStep: 'a96d9f14-3835-4e08-b8a7-a77f3b754886',
+  n8nSecondStep: '0164614d-e044-4c3f-9ef9-9212407bf402',
   n8nFourthStep: 'a5f9547f-905b-4fc0-b5dc-3ebbb5936eda',
-  n8nSeventhStep: '916a955c-467e-4830-9d95-3bb3fc976698',
   n8nSixthStep: 'd9a6bc71-e1da-42a0-96c9-0a29fc92eedc',
+  n8nSeventhStep: '916a955c-467e-4830-9d95-3bb3fc976698',
 };

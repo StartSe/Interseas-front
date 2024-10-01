bd4ba535a4a58594d8ab255828cb2d453e6db97b
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-01 17:07:48 -0300

    chore: update dist build artifacts

--- flowise-embed/dist/constants.d.ts
+++ flowise-embed/dist/constants.d.ts
@@ -6,6 +6,9 @@ export declare const constants: {
     apiUtilsUrl: string;
     n8nDomain: string;
     n8nFirstStep: string;
+    n8nSecondStep: string;
     n8nFourthStep: string;
+    n8nSixthStep: string;
+    n8nSeventhStep: string;
 };
 //# sourceMappingURL=constants.d.ts.map

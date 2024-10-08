c9a14b2452aae88520aec1af2169259491ad670e
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-08 13:59:19 -0300

    feat: add flow prop to project

--- flowise-embed/src/features/full/components/Full.tsx
+++ flowise-embed/src/features/full/components/Full.tsx
@@ -63,6 +63,7 @@ export const Full = (props: FullProps, { element }: { element: HTMLElement }) =>
             showTitle={props.theme?.chatWindow?.showTitle}
             showAgentMessages={props.theme?.chatWindow?.showAgentMessages}
             title={props.theme?.chatWindow?.title}
+            flow={props.theme?.chatWindow?.flow || 'compliance'}
             titleAvatarSrc={props.theme?.chatWindow?.titleAvatarSrc}
             welcomeMessage={props.theme?.chatWindow?.welcomeMessage}
             errorMessage={props.theme?.chatWindow?.errorMessage}

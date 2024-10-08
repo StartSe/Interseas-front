f3f6e419334e99e2a287fcc9289979e78e858884
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-08 13:57:35 -0300

    chore: remove unused files

--- flowise-embed/src/components/bubblesCriticalAnalysis/index.ts
+++
@@ -1,7 +0,0 @@
-export { StarterPromptBubble } from './StarterPromptBubble';
-export { SourceBubble } from './SourceBubble';
-export { LoadingBubble } from './LoadingBubble';
-export { LeadCaptureBubble } from './LeadCaptureBubble';
-export { GuestBubble } from './GuestBubble';
-export { BotBubble } from './BotBubble';
-export { AgentReasoningBubble } from './AgentReasoningBubble';

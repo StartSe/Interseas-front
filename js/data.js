3a2e788d51e2cb063e4ff293bfa22475c8db9022
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-09 17:29:10 -0300

    feat: add data.js with menu items for HTML

---
+++ js/data.js
@@ -0,0 +1,14 @@
+export const menuItems = [
+  {
+    id: "compliance",
+    title: "compliance",
+  },
+  {
+    id: "critical_analysis",
+    title: "Analise critica",
+  },
+  {
+    id: "cost_estimate",
+    title: "cost_estimate",
+  },
+];

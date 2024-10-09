56c2995587b301d1c852c78605fda67f53dc8bef
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-09 17:28:29 -0300

    feat: add MenuItem

---
+++ flowise-embed/src/features/menu/components/MenuItem.tsx
@@ -0,0 +1,17 @@
+export interface MenuItemProps {
+  id: string;
+  title: string;
+  subtitle?: string;
+  selected?: boolean;
+}
+export const MenuItem = (props: MenuItemProps) => {
+  return (
+    <a href={`/${props.id}.html`}>
+      <div class={`menu-item ${props.selected ? 'selected' : ''}`}>
+        <p>
+          <b>{props.title}</b>
+        </p>
+      </div>
+    </a>
+  );
+};

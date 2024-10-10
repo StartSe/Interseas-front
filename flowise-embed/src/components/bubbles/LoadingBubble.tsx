96900804428e67db9d78c69d784a179e43b823f2
luislimaAI <luis.lima@startse.com>
2024-10-10 09:25:12 -0300

    style: refactor style

--- flowise-embed/src/components/bubbles/LoadingBubble.tsx
+++ flowise-embed/src/components/bubbles/LoadingBubble.tsx
@@ -9,19 +9,8 @@ export const LoadingBubble = (props: { typeLoading: string }) => {
     return (
       <div class="flex flex-row justify-start mb-2 items-start host-container" style={{ 'margin-right': '50px' }}>
         <Avatar />
-        <div
-          style={{
-            'margin-left': '0.5rem',
-            padding: '1rem',
-            display: 'flex',
-            'flex-direction': 'column',
-            background: '#ECECEC',
-            'text-align': 'center',
-            'border-radius': '0.375rem',
-          }}
-          class="flex justify-start mb-2 items-center animate-fade-in host-container"
-        >
-          <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full" data-testid="host-bubble">
+        <div class=" flex justify-start mb-2 items-center animate-fade-in host-container">
+          <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble" data-testid="host-bubble">
             <UploadingBubble />
           </span>
         </div>

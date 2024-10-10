71b9ca71246070059663f3df82b550205bf3d652
luislimaAI <luis.lima@startse.com>
2024-10-10 13:53:33 -0300

    refactor: simplify loading code

--- flowise-embed/src/components/bubbles/LoadingBubble.tsx
+++ flowise-embed/src/components/bubbles/LoadingBubble.tsx
@@ -1,41 +1,16 @@
-/* eslint-disable solid/components-return-once */
 import { TypingBubble } from '@/components/TypingBubble';
 import { UploadingBubble } from '@/components/UploadingBubble';
 import { Avatar } from '../avatars/Avatar';
 
 export const LoadingBubble = (props: { typeLoading: string }) => {
-  // eslint-disable-next-line solid/reactivity
-  if (props.typeLoading === 'upload') {
-    return (
-      <div class="flex flex-row justify-start mb-2 items-start host-container" style={{ 'margin-right': '50px' }}>
-        <Avatar />
-        <div class=" flex justify-start mb-2 items-center animate-fade-in host-container">
-          <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble" data-testid="host-bubble">
-            <UploadingBubble />
-          </span>
-        </div>
+  return (
+    <div class="flex flex-row justify-start mb-2 items-start host-container" style={{ 'margin-right': '50px' }}>
+      <Avatar />
+      <div class=" flex justify-start mb-2 items-center animate-fade-in host-container">
+        <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble" data-testid="host-bubble">
+          {props.typeLoading === 'upload' ? <UploadingBubble /> : <TypingBubble />}
+        </span>
       </div>
-    );
-  } else {
-    <div class="flex justify-start mb-2 items-start animate-fade-in host-container">
-      <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble" data-testid="host-bubble">
-        <TypingBubble />
-      </span>
-    </div>;
-    return (
-      <div class="flex flex-row justify-start mb-2 items-start host-container" style={{ 'margin-right': '50px' }}>
-        <Avatar />
-        <div
-          style={{
-            'border-radius': '0.375rem',
-          }}
-          class="flex justify-start mb-2 items-center animate-fade-in host-container"
-        >
-          <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble" data-testid="host-bubble">
-            <TypingBubble />
-          </span>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };

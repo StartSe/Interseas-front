425e06e88b105db0d7e6336e999a5d7b65dbf4b0
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-10-15 10:23:18 -0300

    build: updated artifacts

--- flowise-embed/dist/utils/locationUtils.d.ts
+++ flowise-embed/dist/utils/locationUtils.d.ts
@@ -2,5 +2,6 @@ export declare enum locationValues {
     STATE = "State",
     COUNTRY = "Country"
 }
+export declare const removeAccents: (str: string) => string;
 export declare const normalizeLocationNames: (locationInput: string, locationType: locationValues) => string;
 //# sourceMappingURL=locationUtils.d.ts.map

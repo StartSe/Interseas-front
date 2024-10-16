14b25b744eac29f06364fafd11be2ebf733b1c3b
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-10-16 15:12:39 -0300

    chore: reduce pdf-to-text timeout

--- flowise-embed/src/service/aiUtilsApi.ts
+++ flowise-embed/src/service/aiUtilsApi.ts
@@ -4,8 +4,13 @@ const apiHost = constants.apiUtilsUrl;
 export const pdfToText = async (file: Blob): Promise<Response> => {
   const form = new FormData();
   form.append('file', file);
+
+  const timeout_seconds = 10;
+  const signal = AbortSignal.timeout(timeout_seconds * 1000);
+
   return fetch(`${apiHost}/text-extraction/plain-text`, {
     method: 'POST',
     body: form,
+    signal: signal,
   });
 };

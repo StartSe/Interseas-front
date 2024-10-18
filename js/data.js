d43335981eb61c04e2fd24564f77fc2ad81b8c9c
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-10-18 14:18:11 -0300

    chore: temporarily hide unavailable menu option

--- js/data.js
+++ js/data.js
@@ -3,10 +3,10 @@ export const menuItems = [
     id: "compliance",
     title: "Análise de Compliance",
   },
-  {
-    id: "cost_estimate",
-    title: "Estimativa de Custos",
-  },
+  // {
+  //   id: "cost_estimate",
+  //   title: "Estimativa de Custos",
+  // },
   {
     id: "critical_analysis",
     title: "Análise Crítica",

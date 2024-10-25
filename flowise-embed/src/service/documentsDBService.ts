9f64e80d08645f8d393150434f273b18ccfff2e5
samuelpjacobsen <samuel.jacobsen@startse.com>
2024-10-25 15:53:48 -0300

    chore: removed unnecessary console.log

--- flowise-embed/src/service/documentsDBService.ts
+++ flowise-embed/src/service/documentsDBService.ts
@@ -36,7 +36,6 @@ class DocumentsDBService {
       });
 
       const data = await response.json();
-      console.log('data', data);
       return data;
     } catch (error) {
       console.error('Error checking hash in the database:', error);

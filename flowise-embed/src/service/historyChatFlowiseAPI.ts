73b144ee700e2b9810847ab687257e29a75220da
Weslley-silv4 <weslley.silva@startse.com>
2024-12-05 10:49:52 -0300

    feat: update chat history API to use constants for authorization token and improved endpoint

--- flowise-embed/src/service/historyChatFlowiseAPI.ts
+++ flowise-embed/src/service/historyChatFlowiseAPI.ts
@@ -1,27 +1,16 @@
 import { ChatMessage } from '@/types';
-
+import { constants } from '@/constants';
 
 export class historyChatFlowiseAPI {
-
-		private async getChatHistory(apiHost: string, chatFlowId:string, chatHistoryId: string) {
-				const response = await fetch(`${apiHost}/chatmessage/${chatFlowId}?chatId=${chatHistoryId}`, {
-						method: 'GET',
-						headers: {
-								'Authorization': `${this.jwtTOKEN}`,
-						},
-				});
-				const chatData: ChatMessage[] = await response.json();
-
-				return chatData;
-		}
-
-		public async getChatHistoryByChatId(apiHost:string, chatFlowId: string, chatId: string): Promise<any> {
-				return this.getChatHistory(apiHost, chatFlowId, chatId);
-		}
+  async getChatHistory(apiHost: string, chatFlowId: string, chatHistoryId: string) {
+    const response = await fetch(`${apiHost}/api/v1/chatmessage/${chatFlowId}?chatId=${chatHistoryId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `${constants.flowiseJwtToken}`,
+      },
+    });
+    const chatData: ChatMessage[] = await response.json();
+    return chatData;
+  }
 }
 export default historyChatFlowiseAPI;
-
-
-
-
-

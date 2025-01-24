45259c84a2534f30c0dbbf40c2b4299ee3cfca41
Vinicius Baroni Soares <vinicius.soares@startse.com>
2025-01-24 11:25:10 -0300

    chore: change compliance notes docs order

--- flowise-embed/src/utils/complianceUtils.ts
+++ flowise-embed/src/utils/complianceUtils.ts
@@ -1,76 +1,76 @@
 export type messagesArray = {
   messages: string[];
 };
-export const CCTCOMPLIANCE = `COMPLIANCE NOTES - CCT AÉREO X CONHECIMENTO HAWB
+export const CCTCOMPLIANCE = `COMPLIANCE NOTES - CONHECIMENTO HAWB X CCT AÉREO
 
-- Identificação do conhecimento de carga: [VALOR DO CCT AÉREO]  
+- Identificação do conhecimento de carga: [VALOR DO CCT AÉREO]
     - HAWB - Número do HAWB: [VALOR DO HAWB]
 
-- Data/hora de emissão: [VALOR DO CCT AÉREO]  
+- Data/hora de emissão: [VALOR DO CCT AÉREO]
     - HAWB - Data de emissão do HAWB: [VALOR DO HAWB]
 
 - Identificação para vinculação ADI/DSI eletrônica/DTA/e-DMOV: [VALOR DO CCT AÉREO]
     - HAWB - Número do MAWB/AWB associados: [VALOR DO HAWB]
 
-- Aeroporto de origem: [VALOR DO CCT AÉREO]  
+- Aeroporto de origem: [VALOR DO CCT AÉREO]
     - HAWB - Airport of Departure: [VALOR DO HAWB]
 
-- Aeroporto de destino: [VALOR DO CCT AÉREO]  
+- Aeroporto de destino: [VALOR DO CCT AÉREO]
     - HAWB - Airport of Destination: [VALOR DO HAWB]
 
-- Recinto aduaneiro de destino: [VALOR DO CCT AÉREO]  
+- Recinto aduaneiro de destino: [VALOR DO CCT AÉREO]
     - HAWB - Final Destination (se não constar, igual ao Airport of Destination): [VALOR DO HAWB]
 
-- Quantidade de volumes: [VALOR DO CCT AÉREO]  
+- Quantidade de volumes: [VALOR DO CCT AÉREO]
     - HAWB - Nº of Pieces: [VALOR DO HAWB]
 
-- Peso Bruto: [VALOR DO CCT AÉREO]  
+- Peso Bruto: [VALOR DO CCT AÉREO]
     - HAWB - Gross Weight: [VALOR DO HAWB]
 
 - Informação "Wooden Packing": [VALOR DO CCT AÉREO]
     - HAWB - Informação "Wooden Packing": [VALOR DO HAWB]
 
-- Indicador de presença de partes e peças de madeira maciça: [VALOR DO CCT AÉREO]  
+- Indicador de presença de partes e peças de madeira maciça: [VALOR DO CCT AÉREO]
     - HAWB - Wooden Packing: [VALOR DO HAWB]
 
-- Descrição das mercadorias: [VALOR DO CCT AÉREO]  
+- Descrição das mercadorias: [VALOR DO CCT AÉREO]
     - HAWB - Descrição das mercadorias: [VALOR DO HAWB]
 
-- Moeda de origem: [VALOR DO CCT AÉREO]  
+- Moeda de origem: [VALOR DO CCT AÉREO]
     - HAWB - Moeda do frete: [VALOR DO HAWB]
 
-- Frete por item de carga: [VALOR DO CCT AÉREO]  
+- Frete por item de carga: [VALOR DO CCT AÉREO]
     - HAWB - Frete por peso: [VALOR DO HAWB]
 
-- Forma de pagamento // Por peso/valor: [VALOR DO CCT AÉREO]  
+- Forma de pagamento // Por peso/valor: [VALOR DO CCT AÉREO]
     - HAWB - Frete por peso: [VALOR DO HAWB]
 
-- Totais na moeda de origem: [VALOR DO CCT AÉREO]  
+- Totais na moeda de origem: [VALOR DO CCT AÉREO]
     - HAWB - Valor total do frete: [VALOR DO HAWB]
 
-- Embarcador estrangeiro: [VALOR DO CCT AÉREO]  
+- Embarcador estrangeiro: [VALOR DO CCT AÉREO]
     - HAWB - Shipper: [VALOR DO HAWB]
 
-- País do embarcador estrangeiro: [VALOR DO CCT AÉREO]  
+- País do embarcador estrangeiro: [VALOR DO CCT AÉREO]
     - HAWB - País do Shipper: [VALOR DO HAWB]
 
-- Consignatário / Identificação: [VALOR DO CCT AÉREO]  
+- Consignatário / Identificação: [VALOR DO CCT AÉREO]
     - HAWB - Consignee: [VALOR DO HAWB]
 
-- MAWB/AWB associados: [VALOR DO CCT AÉREO]  
+- MAWB/AWB associados: [VALOR DO CCT AÉREO]
     - HAWB - Conforme consta no HAWB: [VALOR DO HAWB]
 `;
 
 export const CE_MERCANTE = `
-COMPLIANCE NOTES - CE MERCANTE X CONHECIMENTO BL
+COMPLIANCE NOTES - CONHECIMENTO BL X CE MERCANTE
 
-- Número do conhecimento de embarque: [VALOR DO CE MERCANTE]  
+- Número do conhecimento de embarque: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Número do documento: [VALOR DO BL]
 
-- Data de emissão: [VALOR DO CE MERCANTE]  
+- Data de emissão: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Data: [VALOR DO BL]
 
-- Shipper/Exporter: [VALOR DO CE MERCANTE]  
+- Shipper/Exporter: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Shipper: [VALOR DO BL]
 
 - Dados do Consignatário: [VALOR DO CE MERCANTE]
@@ -78,39 +78,39 @@ COMPLIANCE NOTES - CE MERCANTE X CONHECIMENTO BL
 
 - Dados do Notify: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Notify: [VALOR DO BL]
-    
+
 - Navio: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Navio: [VALOR DO BL]
 
-- Porto de origem: [VALOR DO CE MERCANTE]  
+- Porto de origem: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Porto de embarque: [VALOR DO BL]
-    
-- Porto de destino: [VALOR DO CE MERCANTE]  
+
+- Porto de destino: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Porto de desembarque: [VALOR DO BL]
 
-- Tipo de carga: [VALOR DO CE MERCANTE]  
+- Tipo de carga: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Tipo de carga: [VALOR DO BL]
 
-- Peso Bruto: [VALOR DO CE MERCANTE]  
+- Peso Bruto: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Peso Bruto: [VALOR DO BL]
 
-- Cubagem: [VALOR DO CE MERCANTE]  
+- Cubagem: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Cubagem: [VALOR DO BL]
 
-- Descrição da mercadoria: [VALOR DO CE MERCANTE]  
+- Descrição da mercadoria: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Descrição resumida das mercadorias: [VALOR DO BL]
 
-- Frete: [VALOR DO CE MERCANTE]  
+- Frete: [VALOR DO CE MERCANTE]
     - Conhecimento BL - Frete: [VALOR DO BL]
 
-- Taxas: [VALOR DO CE MERCANTE]  
-    - Conhecimento BL - Taxas: [VALOR DO BL]   
+- Taxas: [VALOR DO CE MERCANTE]
+    - Conhecimento BL - Taxas: [VALOR DO BL]
 
-- Valor da Capatazia (THC): [VALOR DO CE MERCANTE]  
+- Valor da Capatazia (THC): [VALOR DO CE MERCANTE]
     - Conhecimento BL - Valor da Capatazia: [VALOR DO BL]
 
-- Dados dos containeres: [VALOR DO CE MERCANTE TRATADO E SEM \n]  
-    - Conhecimento BL - Dados dos containeres: [VALOR DO BL TRATADO E SEM \n]    
+- Dados dos containeres: [VALOR DO CE MERCANTE TRATADO E SEM \n]
+    - Conhecimento BL - Dados dos containeres: [VALOR DO BL TRATADO E SEM \n]
     `;
 
 export const CRT = `

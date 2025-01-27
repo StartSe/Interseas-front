48a337c53d4816fcc083774049dbcab9f245e6df
luislimaAI <luis.lima@startse.com>
2025-01-27 09:19:14 -0300

    refactor: enhance  detail in checklist instructions for various documents

--- flowise-embed/src/utils/fileClassificationUtils.ts
+++ flowise-embed/src/utils/fileClassificationUtils.ts
@@ -102,10 +102,9 @@ export const checklistCertificadoOrigem = `
 • Acordo - acordos comerciais, como exemplo ACE Nº[número]
 • Fatura Comercial (Número) - também chamado de Invoice, Nº Commercial Invoice, Factura Comercial, "factura"/"factura comercial"/"factura e"/"factura de exportacion"/"fat.coml", retornar o número do mesmo
 • Valor Total das mercadorias - Considerando todas as páginas faça a somatório do valor total informado por espécie de mercadoria usando a ferramenta calculator ou apenas recupere o valor total já informado no documento
-• Valor unitário de cada espécie de mercadoria - Considerando todas as páginas é o valor unitário multiplicado pela quantidade das mercadorias (quantidade X valor unitário, separando mercadorias diferentes por ';')
+• Quantidade
 • NCM
-• NALADI/NALADISA - (código de 8 a 12 dígitos, chamado de Código NALADI ou Código NALADISA)
- `;
+• NALADI/NALADISA - (pode ter de 8 a 12 dígitos, **Não confundir com NCM**) `;
 
 export const checklistCommercialInvoice = `
 • Número do documento
@@ -143,10 +142,8 @@ export const checklistCommercialInvoice = `
 • Peso Liquido – (Procure no documento chaves como NetWeight, NW, Peso Neto ou PN acompanhado de valores númericos e unidades de medida de peso)
 • Quantidade de Volumes - pode ser um entre: crate, box, pallets, bags ou outro relacionado ao tema volume
 • Tipo de Volumes - pode ser um entre: crate, box, pallets, bags ou outro relacionado ao tema volume. Se identificar mais de um tipo, retorne "volumes".
-• Descrição EX-tarifário - (no formato "EX-[número]")
-Se mercadoria é máquina ou equipamento
+• Descrição EX-tarifário - (no formato "EX-[número]", se existir no documento retorne todo o conteúdo da descrição(Análise o texto e corrija as palavras que não fazem sentido no português))
 • Número de Série - (se mercadoria é máquina ou equipamento, buscar como SN, NS, S/N, N/S)
-• Marca
 • Modelo
 Conferências:
 • Importação direta - (Deve retornar true apenas se Adquirente for igual ao Importador, se não, false)
@@ -208,7 +205,7 @@ export const checklistConhecimentoHawb = `
 • CNPJ do Consignee - (Consignatário/Identificação)
 • "To order of"
 • Notify - (razão social, endereço, CNPJ e CEP)
-• Frete - Todas as informações referentes a frete. Trazer Label; Trazer tipo (Prepaid/Collect); moeda e valor (Total Prepaid; Total Collect; Total Freight, Basic Ocean Freight; Ocean Freight; O/F; OF; Freight; International freight; Freight and Charges, CAPATAZIA, THD). Trazer todas as informações que encontrar de forma detalhada, organizada com: Label, Tipo, moeda, valor. Traga uma string com todos estes dados. Não converta os atributos internos do frete para json.
+• Frete - Todas as informações referentes a frete. Trazer tipo (Prepaid/Collect); moeda e valor (Total Prepaid; Total Collect; Total Freight, Basic Ocean Freight; Ocean Freight; O/F; OF; Freight; International freight; Freight and Charges, CAPATAZIA, THD). Trazer todas as informações que encontrar de forma detalhada, organizada com: Label, Tipo, moeda, valor. Traga uma string com todos estes dados. Não converta os atributos internos do frete para json.
 • Forma de pagamento do frete
 • Aeroporto de Partida
 • Aeroporto de Destino
@@ -228,7 +225,7 @@ export const checklistConhecimentoHawb = `
 Dados de Compliance:
 • Description of Goods - (Descrição resumida e completa das mercadorias)
 • Forma de pagamento - (Collect/Prepaid, por peso/valor ou outros encargos)
-• Somatório frete e taxas - Considerando todas as páginas, fazer a somatória do 'Valor total do frete' e taxas encontrados no checklist usando a ferramenta calculator
+• Somatório frete e taxas - Considerando todas as páginas, fazer a somatória do 'Valor total do frete' e taxas encontrados
 
 Conferências:
 • Máquina/Equipamento
@@ -335,6 +332,7 @@ export const ChecklistProformaInvoice = `
 • Quantidade de volumes estimada - (crate/box/pallets; Se não encontrar, **apenas** retorne false)
 • Dimensão estimada dos volumes - (referente a crate/box/pallets) (volume x altura x largura)
 Conferências:
+
 • Multiplicação de valor unitário dos itens comercializados -  (Retornar as mercadorias no formato: valor unitário x quantidade comercializada = resultado ()(Dentro de () retorne VALOR TOTAL DE ACORDO se o resultado for igual ao valor total da mercadoria no documento e VALOR TOTAL NÃO ESTÁ DE ACORDO se o resultado for diferente do valor total da mercadoria no documento) Inclua "<br>" para separar as mercadorias)
 • Valor Total das Mercadorias - (Considerando todas as páginas faça a somatório do valor total informado por espécie de mercadoria usando a ferramenta calculator e retorne no formato total mercadoria 1 + total mercadoria 2 + ... + total mercadoria n = resultado ()(Dentro de () retorne VALOR TOTAL DE ACORDO se o resultado for igual ao valor total informado no documento e VALOR TOTAL NÃO ESTÁ DE ACORDO se o resultado for diferente do valor total informado no documento))
 • Máquina/Equipamento`;
